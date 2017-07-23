0732e3d9b9be8092eedd25c7dbcc351fc0b6bb1c
Jay Harris <jaywritescode@users.noreply.github.com>
2017-07-23 14:42:36 -0400

    fix null -> empty string conversion

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ module.exports = function to_sentence(arr, options) {
 
   switch(arr.length) {
   case 0:
-    return "";
+    return '';
   case 1:
-    return arr[0].toString();
+    return arr[0] + '';
   case 2:
-    return `${arr[0]}${opts.two_words_connector}${arr[1]}`;
+    return `${arr[0]}${empty(opts.two_words_connector)}${arr[1]}`;
   default:
-    return `${arr.slice(0, -1).join(opts.words_connector)}${opts.last_word_connector}${arr.slice(-1)}`;
+    return `${arr.slice(0, -1).join(empty(opts.words_connector))}${empty(opts.last_word_connector)}${arr.slice(-1)}`;
   }
+}
+
+function empty(str) {
+  return str || '';
 }
